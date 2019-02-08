62db0707a046bfbe423480849d284e83519ee31d
Steven Capuzzi <stevenczz@gmail.com>
2019-02-08 17:31:37 -0500

    adds state to the Board component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+      }
     render() {
       return (
-        <button className="square" onClick={function() { alert('click'); }}>
-          {this.props.value}
+        <button className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
